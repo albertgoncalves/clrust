589a55c65c2b24ff540238b76390b02e7d8a0d20
albertgoncalves <goncalves.albert@gmail.com>
2020-01-20 13:07:59 -0500

    clippy to *all* targets

--- rust/src/test.rs
+++ rust/src/test.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
-mod test {
+mod geom {
+    use std::f32;
+
     #[test]
     fn distance_f32() {
         macro_rules! assert_distance {
@@ -8,7 +10,10 @@ mod test {
                     crate::geom::Point { x: $ax, y: $ay };
                 let b: crate::geom::Point =
                     crate::geom::Point { x: $bx, y: $by };
-                assert_eq!(crate::geom::distance_f32(a, b), $d)
+                assert!(
+                    (crate::geom::distance_f32(a, b) - $d).abs()
+                        < f32::EPSILON,
+                )
             };
         }
         assert_distance!(0.0, 0.0, 3.0, 4.0, 5.0);
@@ -17,6 +22,26 @@ mod test {
         assert_distance!(5.0, 12.0, 0.0, 0.0, 13.0);
     }
 
+    #[test]
+    fn bounds() {
+        assert_eq!(
+            crate::geom::bounds(&[
+                crate::geom::Point { x: 0.0, y: 1.0 },
+                crate::geom::Point { x: 9.0, y: -1.0 },
+                crate::geom::Point { x: -3.0, y: 10.0 },
+                crate::geom::Point { x: 12.0, y: 5.0 },
+            ]),
+            crate::geom::Bounds {
+                min_x: -3.0,
+                max_x: 12.0,
+                min_y: -1.0,
+                max_y: 10.0,
+            }
+        );
+    }
+}
+
+mod main {
     #[test]
     fn row_to_point() {
         macro_rules! assert_row_to_point {
@@ -37,25 +62,9 @@ mod test {
         assert_row_to_point!("0,1,2,3", 4, 0, 0);
         assert_row_to_point!("0,1,2,3", 4, 1, 4);
     }
+}
 
-    #[test]
-    fn bounds() {
-        assert_eq!(
-            crate::geom::bounds(&[
-                crate::geom::Point { x: 0.0, y: 1.0 },
-                crate::geom::Point { x: 9.0, y: -1.0 },
-                crate::geom::Point { x: -3.0, y: 10.0 },
-                crate::geom::Point { x: 12.0, y: 5.0 },
-            ]),
-            crate::geom::Bounds {
-                min_x: -3.0,
-                max_x: 12.0,
-                min_y: -1.0,
-                max_y: 10.0,
-            }
-        );
-    }
-
+mod math {
     #[test]
     fn average_f32() {
         assert_eq!(crate::math::average_f32(&[1.0, 2.0, 3.0]), Some(2.0));
